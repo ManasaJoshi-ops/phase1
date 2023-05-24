efd3460c656590b2cee3f4717802fd426d7d9ef2
Manasa Joshi <manasa.joshi10@gmail.com>
2023-05-25 02:22:03 +0530

    datatypes program

---
+++ Javaphase1/src/basics/datatypes.java
@@ -0,0 +1,33 @@
+package basics;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		byte a=(byte) 129;
+		System.out.println(a);
+		
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		
+		char ch='a';
+		System.out.println(ch);
+		int x=ch;
+		System.out.println(x);
+		
+		
+		int y=119;
+		char x1=(char) y;
+		System.out.println(x1);
+		
+		char ch1='w';
+		int y1=ch1;
+		System.out.println(y1);
+		
+	}
+}
