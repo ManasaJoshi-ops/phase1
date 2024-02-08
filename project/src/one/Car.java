bbe2de5ea68f576c6e2fc9997ec409ac6a943716
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2024-02-09 03:48:42 +0530

    difference between accessing static and non static variable

---
+++ project/src/one/Car.java
@@ -0,0 +1,15 @@
+package one;
+
+public class Car {
+	
+	public String Color;
+	public int numOfWheels;
+	
+	public static String manufacturer="Honda";
+
+	public Car() {
+		Color="white";
+		numOfWheels=4;
+		
+	}
+}
--- project/src/one/demo3.java
+++ project/src/one/demo3.java
@@ -6,8 +6,15 @@ public class demo3 {
 		System.out.println("from main method");
 		//create instance of class to print the contents and non static block
 		
-		demo3 obj=new demo3();
-		demo3 obj2=new demo3();
+//		demo3 obj=new demo3();
+//		demo3 obj2=new demo3();
+		Car car=new Car();
+		System.out.println(car.Color);
+		System.out.println(car.numOfWheels);
+		System.out.println(Car.manufacturer);
+		//static variable(so can be printed with class name
+		
+		
 	}
 	//constructor
 	public demo3() {
