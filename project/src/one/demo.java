1fadaf00b6331244700c570d15653249a290b8ba
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2024-02-09 01:42:55 +0530

    static block program

---
+++ project/src/one/demo.java
@@ -0,0 +1,15 @@
+package one;
+
+public class demo {
+	public static void main(String args[]) {
+		System.out.println("From main method");
+	}
+	static {
+		System.out.println("from static block1");
+	}
+	static {
+		System.out.println("from static block2");
+	}
+	
+
+}
