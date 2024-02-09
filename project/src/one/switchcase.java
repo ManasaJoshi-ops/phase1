5b0b7a1758afcfabbc936764321eb0e675b616f1
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2024-02-10 04:05:48 +0530

    switch case

---
+++ project/src/one/switchcase.java
@@ -0,0 +1,22 @@
+package one;
+
+public class switchcase {
+	public static void main(String[] args) {
+		int UserOptions=1;
+		
+		switch(UserOptions) {
+		case 1:System.out.println("Main Menu");
+		break;
+		case 2:System.out.println("delete option");
+		break;
+		case 3:System.out.println("Back");
+		break;
+		case 4:System.out.println("Next page");
+		break;
+		default:System.out.println("close screen");
+		
+		
+		}
+	}
+
+}
