723a4bf2c4d4ba9fa8f16e6f142cce35c0043803
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2024-02-16 13:24:45 +0530

    final keyword explained

---
+++ project/src/finalkey/FinalKeyword.java
@@ -0,0 +1,12 @@
+package finalkey;
+
+public final class FinalKeyword {
+	
+     public static void main(String[] args) {
+	
+    	 //final in inheritance---->> cannot be inherited(if you mark a class as final it cannot be implemented
+    	 //so abstract class can never be final (as it has be inherited)
+    	 //final in variables---->> cannot be changed
+    	 
+}
+}
